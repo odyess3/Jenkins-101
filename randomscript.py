c06aa0dec88ac52419fa926c8e31c4b39c174351
Ody Nwagwu (260004669) <Ody.Nwagwu@gevernova.com>
2025-11-13 14:56:45 +0000

    fixing the rest

--- randomscript.py
+++ randomscript.py
@@ -1,7 +1,7 @@
 import sys
 
 print(f"Hello {sys.argv[1]}")
-print("this is just random stuff here a text \n ${params.TEXT}")
-print("The toggle of this value is ${params.BOOLEAN}")
-print("Your choice is ${params.CHOICE}")
-print("Finally the password is ${params.PASSWORD}")
+print(f"this is just random stuff here a text \n {sys.argv[2]}")
+print(f"The toggle of this value is {sys.argv[3]}")
+print(f"Your choice is {sys.argv[4]}")
+print(f"Finally the password is {sys.argv[5]}")
