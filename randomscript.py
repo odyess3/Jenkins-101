d447a939bf60266fc96daac4503733583c3fd146
Ody Nwagwu (260004669) <Ody.Nwagwu@gevernova.com>
2025-11-13 14:49:43 +0000

    test

--- randomscript.py
+++ randomscript.py
@@ -1,7 +1,7 @@
-import system
+import sys
 
-printf ("Hello {sys.argv[1]}")
+print(f"Hello {sys.argv[1]}")
 print("this is just random stuff here a text \n ${params.TEXT}")
-print ("The toggle of this value is ${params.BOOLEAN}")
+print("The toggle of this value is ${params.BOOLEAN}")
 print("Your choice is ${params.CHOICE}")
 print("Finally the password is ${params.PASSWORD}")
